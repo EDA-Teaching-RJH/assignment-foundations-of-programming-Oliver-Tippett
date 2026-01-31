6a52942ace17b386748f42f15651555e801b9878
Oliver-Tippett <ot11@kent.ac.uk>
2026-01-31 14:46:32 +0000

    display_menu choice

--- fleet_manager.py
+++ fleet_manager.py
@@ -5,3 +5,19 @@ def init_database():
     ids = ["100","101","102","103","104"]
 
     return names, ranks, division, ids
+
+def display_menu():
+    user = input("Enter your full name: ")
+    print("Current student logged in is",user)
+    print("1. Add Crew Member")
+    print("2. Remove Crew Member")
+    print("3. Update Rank")
+    print("4. Display Crew Roster")
+    print("5. Search Crew")
+    print("6. Filter by Division")
+    print("7. Calculate Payroll")
+    print("8. Count Officers")
+    print("9. Exit")
+
+    choice = input("Select Option (1-9)")
+    return choice
